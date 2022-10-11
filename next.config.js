da62266c0ed9b1cceb3c5183f02fdd5ddfab936e
Huyett <Todd.Huyett@estes-express.com>
2022-10-10 21:58:45 -0400

    'd'

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  assetPrefix: isProd ? '/toddhuyett/' : '',
   images: {
     unoptimized: true,
   },
